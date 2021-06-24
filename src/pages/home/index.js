b3afbbdce03dfab88bda074ffe4ac1ca78dd12a0
adisurya <marjaya@undiksha.ac.id>
2021-06-24 20:45:25 +0800

    redux

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import {signOut} from '../../controller/loginController'
 
 export default function home () {
+    function signedOut(navigation){
+        navigation.navigate("Login")
+    }
     return (
-        <View>
-            <Text>anda sudah login</Text>
+        <View style={styles.teks}>
+            <Text >anda sudah login</Text>
+            <View>
+                <TouchableOpacity >
+                    <Text>ini log out</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white'
+    },
+    teks: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
 
